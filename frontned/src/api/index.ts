16af3a4946af9f84ebba60dc9bb09d8ff743f8da
yedad <yedad@qq.com>
2026-05-02 13:47:15 +0800

    docs(api): 添加部署环境流媒体服务地址注释
    添加被注释掉的部署环境流媒体服务地址，方便后续部署时快速切换

--- frontned/src/api/index.ts
+++ frontned/src/api/index.ts
@@ -7,4 +7,7 @@ const request = axios.create({
 
 export const STREAM_BASE_URL = 'http://127.0.0.1:8000'
 
+// 部署环境下的流媒体服务地址
+// export const STREAM_BASE_URL = 'http://10.0.0.8'
+
 export default request
