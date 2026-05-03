a7dd01d59e484b1d0bb6f159dbf952efbf6c3e6d
yedad <yedad@qq.com>
2026-05-03 12:08:00 +0800

    feat(icons): 添加MDI图标集支持
    在项目中引入@iconify-json/mdi依赖并初始化图标集合，以扩展可用的图标资源

--- frontned/src/main.ts
+++ frontned/src/main.ts
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { addCollection } from '@iconify/vue'
+import icons from '@iconify-json/mdi/icons.json'
 
 import './style.css'
 import App from './App.vue'
 import router from './router'
+
+addCollection(icons)
 
 const app = createApp(App)
 
